957a7de548a439d41ff61b3592dea3fd9c8bd30c
Balachandar-Sivkumar <balachandarsdckap@gmail.com>
2026-01-29 10:05:05 +0530

    static log page added

--- frontend/src/routes/route.js
+++ frontend/src/routes/route.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route,Routes } from "react-router-dom";
-import MainModel from "../components/mainModel";
+import MainModel from "../components/ProjectAccessKeyMain";
 
 ///Project Route function
 function AppRoutes(){
